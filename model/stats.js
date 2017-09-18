4253cbf6055c840ae03e6fca4feabf17120f2ff3
Meepedy <david@psyon.org>
2017-09-18 10:54:00 -0500

    added stat defaults

--- model/stats.js
+++ model/stats.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const statSchema = Schema({
-  strength: { type: Number, required: true},
-  dexterity: { type: Number, required: true},
-  constitution: { type: Number, required: true},
-  intelligence: { type: Number, required: true},
-  charisma: { type: Number, required: true},
-  wisdom: { type: Number, required: true}
+  strength: { type: Number, required: true, default: 3},
+  dexterity: { type: Number, required: true, default: 3},
+  constitution: { type: Number, required: true, default: 3},
+  intelligence: { type: Number, required: true, default: 3},
+  charisma: { type: Number, required: true, default: 3},
+  wisdom: { type: Number, required: true, default: 3}
 });
 
 module.exports = mongoose.models.stat || mongoose.model('stat', statSchema);
