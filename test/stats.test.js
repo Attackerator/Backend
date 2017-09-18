2bb0fd27febbf157e215e55b0e323396c1a171b3
Meepedy <david@psyon.org>
2017-09-18 16:09:19 -0500

    make linter happy

--- test/stats.test.js
+++ test/stats.test.js
@@ -1,10 +1,7 @@
 'use strict';
 
-const debug = require('debug')('app:test/user.test');
 const Stats = require('../model/stats');
 const { expect } = require('chai');
-
-const helper = require('./test-helper')
 
 require('../lib/mongoose-connect');
 
@@ -20,14 +17,14 @@ const testBody = {
 describe('stats creation', function() {
   describe('create stats', function() {
     it('should return stats', function() {
-      return Stats.createStats(helper.testStats)
+      return Stats.createStats(testBody)
         .save()
         .then(saved => {
-          expect(saved.strength).to.deep.equal(3);
-          expect(saved.dexterity).to.deep.equal(3);
-          expect(saved.constitution).to.deep.equal(3);
-          expect(saved.intelligence).to.deep.equal(3);
-          expect(saved.charisma).to.deep.equal(3);
+          expect(saved.strength).to.deep.equal(4);
+          expect(saved.dexterity).to.deep.equal(5);
+          expect(saved.constitution).to.deep.equal(7);
+          expect(saved.intelligence).to.deep.equal(23);
+          expect(saved.charisma).to.deep.equal(2);
           expect(saved.wisdom).to.deep.equal(3);
 
 
