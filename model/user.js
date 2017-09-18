1871f242367144c49b4101a9d00031a56715e6e8
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-18 10:11:13 -0500

    userSchema

--- model/user.js
+++ model/user.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const createError = require('http-errors');
+const debug = require('debug')('app:model/user');
+const jwt = require('jsonwebtoken');
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  username: {type: String,required: true, unique: true}
+  ,email: {type: String, required: true, unique: true}
+  ,password: {type: String}
+  ,findHash: {type: String, unique: true}
+});
+
+module.exports = mongoose.models.user || mongoose.model('user', userSchema);
