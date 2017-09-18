6bd2a7e1a6e7c2853912fd140c77c70dab1b1585
nerdbetter <jasontlogan@gmail.com>
2017-09-18 10:12:12 -0500

    express listener set up

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const debug = require('debug')('app:server');
+
+//require('dotenv').load();
+
+const app = express();
+
+require('./lib/mongoose-connect');
+
+app.use(morgan('dev'));
+app.use(cors());
+
+const PORT = process.env.PORT;
+if (!module.parent){
+  if (!PORT){
+    throw new Error('Forgot to specify PORT');
+  }
+  app.listen(PORT,function(){
+    debug(`Listening on PORT ${PORT}`);
+  });
+}
+
+module.exports = app;
