9d3e24553a3b6eaae6f08ca34da7007786cc98ba
Dustin <dyelland@gmail.com>
2017-09-18 10:53:26 -0500

    built character constructor and added static createCharacter method

---
+++ model/character.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+const debug = require('debug')('app:model/character');
+
+const characterSchema = {
+  user: { type: Schema.Types.ObjectId, required: true },
+  name: { type: String, required: true },
+  stats: { type: Schema.Types.ObjectId },
+  skills: { type: Schema.Types.ObjectId },
+  saves: { type: Schema.Types.ObjectId },
+  spells: { type: Schema.Types.ObjectId },
+  attack: { type: Schema.Types.ObjectId },
+  sayings: { type: Schema.Types.ObjectId }
+};
+
+const Character = module.exports = mongoose.models.character || mongoose.model('Character',characterSchema);
+
+Character.statics.createCharacter = function(character){
+  debug(character);
+  return new Character(...character).save();
+};
