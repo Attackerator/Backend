8958a30204df12f805fc0c62838e49383b5f1424
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-19 14:27:09 -0500

    fix spell-route.test.js to use bearer auth

--- test/spell-route.test.js
+++ test/spell-route.test.js
@@ -6,17 +6,23 @@ const { expect } = require('chai');
 const debug = require('debug')('app:test/spell');
 require('../lib/mongoose-connect');
 const helper = require('./test-helper');
-
+const User = require('../model/user.js');
 
 describe('Spell Routes',function(){
-
+  beforeEach(function () {
+    return User.createUser(helper.user)
+      .then(user => this.testUser = user)
+      .then(user => user.generateToken())
+      .then(token => this.testToken = token);
+  });
   afterEach(function(){
-    return helper.kill;
+    return helper.kill();
   });
   describe('POST /api/spell',function(){
     it('should return 200 if it saves a new spell',function(){
       return request.post(`/api/spell`)
         .send(helper.spell)
+        .set({'Authorization': `Bearer ${this.testToken}`})
         .expect(200)
         .expect(res => {
           debug(res.body.name);
@@ -27,6 +33,7 @@ describe('Spell Routes',function(){
     it('should return 400 if no body is provided',function(){
       return request.post('/api/spell')
         .send()
+        .set({'Authorization': `Bearer ${this.testToken}`})
         .expect(400);
     });
   });
