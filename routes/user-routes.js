b5ea69468ef3e6478d42f2526ddc99a8d081a3ff
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-19 11:21:37 -0500

    route for GET /api/signin ;

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -6,8 +6,19 @@ const Router = require('express').Router;
 
 const User = require('../model/user');
 
+const basicAuth = require('../lib/basic-auth-middleware');
+
 const router = module.exports = new Router();
 
+router.get('/api/signin', basicAuth, function(req,res,next){
+  debug('GET /api/signin');
+
+  User.findOne({username: req.auth.username})
+    .then(user => user.comparePasswordHash(req.auth.password))
+    .then(user => user.generateToken())
+    .then(token => res.send(token))
+    .catch(next);
+});
 
 router.post('/api/user', jsonParser, function (req,res,next){
   debug('POST /api/user');
