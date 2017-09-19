38b97c4209e6d01ee0638d00b84dd86a54755880
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-19 14:28:44 -0500

    fix stats-route.test.js to use bearer auth

--- test/stats-route.test.js
+++ test/stats-route.test.js
@@ -5,19 +5,28 @@ const request = require('supertest')(app);
 const { expect } = require('chai');
 
 require('../lib/mongoose-connect');
+const helper = require('./test-helper');
+const User = require('../model/user.js');
 
-const Stats = require('../model/stats');
+
 
 describe('stats routes', function() {
   describe('POST /api/stats', function() {
     describe('with a valid body', function() {
-      after(function () {
-        return Stats.remove({});
+      beforeEach(function () {
+        return User.createUser(helper.user)
+          .then(user => this.testUser = user)
+          .then(user => user.generateToken())
+          .then(token => this.testToken = token);
+      });
+      afterEach(function () {
+        return helper.kill();
       });
 
       it('should return stats', function () {
         return request
           .post('/api/stats')
+          .set({'Authorization': `Bearer ${this.testToken}`})
           .send({ strength: 3})
           .expect(200)
           .expect(res => {
