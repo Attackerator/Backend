038cc11592c32afa160f254b3d70a9b485d8ab05
nerdbetter <jasontlogan@gmail.com>
2017-09-19 10:35:30 -0500

    added post test for spells

--- test/test-helper.js
+++ test/test-helper.js
@@ -4,8 +4,18 @@ const User = require('../model/user');
 const Stats = require('../model/stats');
 const Character = require('../model/character');
 const debug = require('debug')('app:test-helper');
+const Spell = require('../model/spells');
 
-
+const exampleSpell = {
+  name: 'Donkey Fart',
+  toHitBonus: 4,
+  damageBonus: 7,
+  damageType: 'necrotic',
+  diceType: '8',
+  diceCount: '5',
+  description: 'Fills a sixty foot area of effect centered on caster. Everything in effected area rolls fortitude check. Half damage on miss.',
+};
+const testSpell = Spell.createSpell(exampleSpell);
 
 const exampleBody = {
   username: 'MrDonkey1028'
@@ -31,18 +41,20 @@ debug('testUser');
 //const testStats = Stats.createStats(exampleStats);
 //debug('testStats');
 const testCharacter = Character.createCharacter(exampleCharacter);
-debug('testUser');
+debug('testCharacter');
 
 const deleteCharacter = function(){
   Promise.all([
     User.remove({}),
     //Stats.remove({}),
     Stats.remove({}),
+    Spell.remove({}),
     Character.remove({}),
   ]);
 };
 
 module.exports = {
+  spell: testSpell,
   user: testUser,
   //stats: testStats,
   character: testCharacter,
